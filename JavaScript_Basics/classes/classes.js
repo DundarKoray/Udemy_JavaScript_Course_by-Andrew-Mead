4321487096e88ab56959d7263fdd1a224bb7ea23
Koray Dundar <koraydundar_40@hotmail.com>
2019-04-09 11:13:13 +0300

    Did practices about classes

---
+++ JavaScript_Basics/classes/classes.js
@@ -0,0 +1,36 @@
+class PersonClass {
+    constructor(firstName, lastName, age, likes =[]){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+        this.point = 0
+    }
+    get increasePoint() {
+        this.point = this.point + 5
+    }
+
+    decreasePoint(x) {
+        this.point = this.point - x
+    }
+
+
+}
+
+const personKoray = new PersonClass('Koray', 'Dündar', 29, ['Coding', 'Scuba-Diving', 'Fishing', 'Playing guitar'])
+console.log(personKoray)
+
+const personSimon = new PersonClass('Simon', 'Maher', 40, ['Swimming', 'Cooking'])
+console.log(personSimon)
+
+const personHari = new PersonClass('Hari', 'Basnet', 26, ['Running', 'Singin', 'Driving'], 24)
+console.log(personHari)
+
+personKoray.increasePoint
+console.log(personKoray);
+
+personHari.decreasePoint(3)
+console.log(personHari);
+
+personKoray.decreasePoint(2)
+console.log(personKoray);
