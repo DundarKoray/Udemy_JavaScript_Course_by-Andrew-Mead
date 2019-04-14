6c6b5dfa7ee643c34c949c2fb89fb0ed275d30ee
Koray Dundar <koraydundar_40@hotmail.com>
2019-04-14 15:57:36 +0300

    refactoring

---
+++ JavaScript_DOM_Exercise_ToDoApp/toDoList/todo-app.js
@@ -0,0 +1,29 @@
+let todos = getSavedTodos()
+
+const filters = {
+    searchText: '',
+    hideCompleted: false
+}
+
+renderTodos(todos, filters)
+
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+document.querySelector('#new-todo').addEventListener('submit', function (e){
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.text.value,
+        completed: false
+    })
+    saveTodos(todos)
+    renderTodos(todos, filters)
+    e.target.elements.text.value = '';
+})
+
+document.querySelector('#hide-completed').addEventListener('change', function(e) {
+    filters.hideCompleted = e.target.checked
+    renderTodos(todos, filters)
+})
