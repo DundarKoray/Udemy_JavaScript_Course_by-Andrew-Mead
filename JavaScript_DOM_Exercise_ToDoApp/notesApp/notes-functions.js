061ef47ad7e34714ef01e6e0f162073f83903dbd
Koray Dundar <koraydundar_40@hotmail.com>
2019-04-15 12:45:24 +0300

    dom for remove button

--- JavaScript_DOM_Exercise_ToDoApp/notesApp/notes-functions.js
+++ JavaScript_DOM_Exercise_ToDoApp/notesApp/notes-functions.js
@@ -16,13 +16,21 @@ const saveNotes = function (notes){
 
 // Generate the DOM structure for a note
 const generateNoteDOM = function (note) {
-    const noteEl = document.createElement('p')
+    const noteEl = document.createElement('div')
+    const textEl = document.createElement('span')
+    const button = document.createElement('button')
 
+    // Setup the remove note button
+    button.textContent = 'x'
+    noteEl.appendChild(button)
+
+    // Setup the note title text
         if (note.title.length > 0) {
-            noteEl.textContent = note.title
+            textEl.textContent = note.title
         }   else {
-            noteEl.textContent = 'Unnamed note'
+            textEl.textContent = 'Unnamed note'
         }
+        noteEl.appendChild(textEl)
 
         return noteEl
 }
--- JavaScript_DOM_Exercise_ToDoApp/toDoList/todo-functions.js
+++ JavaScript_DOM_Exercise_ToDoApp/toDoList/todo-functions.js
@@ -38,9 +38,24 @@ const renderTodos = function (todos, filters) {
 
 // Get the DOM elements for an individual note
 const generateTodoDOM = function (todo){
-    const p = document.createElement('p')
-    p.textContent = todo.text; 
-    return p
+   const todoEl = document.createElement('div')
+   const checkbox = document.createElement('input')
+   const todoText = document.createElement('span')
+   const removeButton = document.createElement('button')
+   
+   // Setup todo checkbox
+   checkbox.setAttribute('type', 'checkbox')
+   todoEl.appendChild(checkbox)
+
+   // Setup the todo text
+   todoText.textContent = todo.text
+   todoEl.appendChild(todoText)
+   
+   // Setup the remove button
+   removeButton.textContent = 'x'
+   todoEl.appendChild(removeButton)
+
+   return todoEl
 }
 
 // Get the DOM elements for list summary
