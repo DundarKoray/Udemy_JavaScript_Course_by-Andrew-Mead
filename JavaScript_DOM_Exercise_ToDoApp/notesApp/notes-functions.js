df01d9a3185e18b5ffef3c926dfab51368202d44
Koray Dundar <koraydundar_40@hotmail.com>
2019-04-16 13:09:00 +0300

    implementing checkbox and edit function

--- JavaScript_DOM_Exercise_ToDoApp/notesApp/notes-functions.js
+++ JavaScript_DOM_Exercise_ToDoApp/notesApp/notes-functions.js
@@ -25,10 +25,11 @@ const removeNote = function (id) {
     }
 }
 
+
 // Generate the DOM structure for a note
 const generateNoteDOM = function (note) {
     const noteEl = document.createElement('div')
-    const textEl = document.createElement('span')
+    const textEl = document.createElement('a')
     const button = document.createElement('button')
 
     // Setup the remove note button
@@ -46,6 +47,7 @@ const generateNoteDOM = function (note) {
         }   else {
             textEl.textContent = 'Unnamed note'
         }
+        textEl.setAttribute('href', './edit.html')
         noteEl.appendChild(textEl)
 
         return noteEl
