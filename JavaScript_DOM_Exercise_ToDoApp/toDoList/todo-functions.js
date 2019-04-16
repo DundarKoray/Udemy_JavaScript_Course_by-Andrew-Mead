c1b325abbd46036dcee66cdb508ecad134bd8881
Koray Dundar <koraydundar_40@hotmail.com>
2019-04-16 09:17:15 +0300

    checkbox is added

--- JavaScript_DOM_Exercise_ToDoApp/toDoList/todo-functions.js
+++ JavaScript_DOM_Exercise_ToDoApp/toDoList/todo-functions.js
@@ -25,6 +25,17 @@ const removeTodo = function (id) {
     }
 }
 
+// Toggle the completed value for a given todo
+const toggleTodo = function (id) {
+    const todo = todos.find(function (todo) {
+        return todo.id === id
+    })
+
+    if (todo !== undefined) {
+        todo.completed = !todo.completed
+    }
+}
+
 //Render application todos based on filters
 const renderTodos = function (todos, filters) {
     const filteredTodos = todos.filter(function (todo) {
@@ -56,7 +67,13 @@ const generateTodoDOM = function (todo){
    
     // Setup todo checkbox
     checkbox.setAttribute('type', 'checkbox')
+    checkbox.checked = todo.completed
     todoEl.appendChild(checkbox)
+    checkbox.addEventListener('change', function () {
+        toggleTodo(todo.id)
+        saveTodos(todos)
+        renderTodos(todos, filters)
+    })
 
     // Setup the todo text
     todoText.textContent = todo.text
@@ -67,7 +84,7 @@ const generateTodoDOM = function (todo){
     todoEl.appendChild(removeButton)
     removeButton.addEventListener('click', function () {
         removeTodo(todo.id)
-        saveTodo(todos)
+        saveTodos(todos)
         renderTodos(todos, filters)
     })
 
